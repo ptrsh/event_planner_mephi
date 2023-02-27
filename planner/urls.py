80c89c40d598aee45b5aa5f8bccdccd3f3fbee73
Kirill Petrushin <chirik@laptop.localdomain>
2023-02-27 14:37:29 +0300

    fix urls; add new app

--- planner/urls.py
+++ planner/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from events.views import EventViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'events', EventViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('admin/', admin.site.urls)
 ]
